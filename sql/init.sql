b3a61f98612bd7591189d1af7d0260421cc1ce51
Your Name <you@example.com>
2021-02-01 19:24:29 +0100

    Edit SQL

--- sql/init.sql
+++ sql/init.sql
@@ -2,23 +2,39 @@ DROP TABLE IF EXISTS cache;
 
 CREATE TABLE cache
 (
-    id_cache  VARCHAR(36) PRIMARY KEY,
-    lieu      VARCHAR(256) NOT NULL,
-    latitude  FLOAT NULL,
-    longitude FLOAT NULL,
-    id_user   VARCHAR(256),
-    type      VARCHAR(32),
-    nature    VARCHAR(32),
-    etat      VARCHAR(32)
+    id_cache       VARCHAR(36) PRIMARY KEY,
+    id_utilisateur VARCHAR(36),
+    lieu           VARCHAR(256) NOT NULL,
+    latitude       FLOAT NULL,
+    longitude      FLOAT NULL,
+    type           VARCHAR(32),
+    nature         VARCHAR(32),
+    etat           VARCHAR(32),
+    CONSTRAINT cache_utilisateur_fk
+        FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id_utilisateur)
 );
 
 DROP TABLE IF EXISTS visite;
 
 CREATE TABLE visite
 (
-    id_visite   VARCHAR(36) PRIMARY KEY,
-    id_cache    VARCHAR(36),
-    commentaire VARCHAR(256),
-    date        DATETIME,
-    photo       VARCHAR(256),
+    id_visite      VARCHAR(36) PRIMARY KEY,
+    id_cache       VARCHAR(36),
+    id_utilisateur VARCHAR(36),
+    commentaire    VARCHAR(256),
+    date           DATETIME,
+    photo          VARCHAR(256),
+    CONSTRAINT visite_cache_fk
+        FOREIGN KEY (id_cache) REFERENCES cache (id_cache),
+    CONSTRAINT visite_utilisateur_fk
+        FOREIGN KEY (id_utilisateur) REFERENCES utilisateur (id_utilisateur),
+);
+
+DROP TABLE IF EXISTS utilisateur;
+
+CREATE TABLE utilisateur
+(
+    id_utilisateur VARCHAR(36) PRIMARY KEY,
+    description    VARCHAR(256),
+    photo          VARCHAR(256)
 );
